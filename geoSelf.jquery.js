c525697aeab8362d267db8d916e85bbc55617353
Lucas Fogliarini <lucasfogliarini@gmail.com>
2014-07-31 19:13:43 -0300

    New structure and features

--- geoSelf.jquery.js
+++ geoSelf.jquery.js
@@ -1,40 +1,61 @@
 $(function(){
     $.fn.geoSelf = function(options){
-      var _inputElem = this[0];
-      $.fn.geoSelf.defaultOptions = {
-        gmapOptions: { componentRestrictions: {country: 'br'} },
-        validation: { code: 01010101010, valid: false, message: 'geometry fail'}
+      var inputElement = this[0];
+      //props
+      inputElement.defaultOptions = {
+        gmapOptions: { componentRestrictions: {country: 'br'} }
       }
+      inputElement.options = $.extend(inputElement.defaultOptions,options);
+      inputElement.gmapAutocomplete = new google.maps.places.Autocomplete(inputElement, inputElement.options);
+      inputElement.$toggle = $('<input id="geoSelf_toggle" type="checkbox" />');
+      inputElement.$lat = $('<input id="geoSelf_lat" type="hidden" />');
+      inputElement.$lng = $('<input id="geoSelf_lng" type="hidden" />');
+      //endprops
 
-      $.fn.geoSelf.options = $.extend($.fn.geoSelf.defaultOptions,options);
-      //validation
-      _inputElem.validation = Array.isArray(_inputElem.validation) ? _inputElem.validation : [];
-      _inputElem.validation.push($.fn.geoSelf.options.validation);
-      //endvalidation
-
-      _inputElem.gmapAutocomplete = new google.maps.places.Autocomplete(_inputElem, $.fn.geoSelf.options);
-      _inputElem.gmapPlace = {};
-
-      function validate(valid){
-         _inputElem.validation.filter(function(elem){
-            if(elem.code == $.fn.geoSelf.options.validation.code) return elem;
-          })[0].valid = valid;
+      //methods
+      inputElement.clearLocation = function(){
+        toggleInputs(false);
       }
+      inputElement.isValid = function(){
+        return inputElement.$lat.val() !== '';
+      }
+      //endmethods
 
-      var $toggle = $('<input id="geoSelf_toggle" type="checkbox" />');
-      this.after($toggle);
-      $toggle.change(function(){
-        _inputElem.gmapPlace = {};
-        _inputElem.disabled = false;
-        validate(false);
-      });
+      //privates
+        //mounter
+        this.after(inputElement.$toggle);
+          inputElement.$toggle.change(inputElement.clearLocation);
+        this.after(inputElement.$lat);
+        this.after(inputElement.$lng);
+        //endmounter
+        //methods
+        function toggleInputs(valid){
+          inputElement.disabled = valid;
+          inputElement.$toggle[0].checked = valid;
+          if(!valid){
+            inputElement.$lat.val('');
+            inputElement.$lng.val('');
+          }
+        }
 
-      google.maps.event.addListener(_inputElem.gmapAutocomplete, 'place_changed', function(){
-        _inputElem.gmapPlace = _inputElem.gmapAutocomplete.getPlace();
-        var invalid = _inputElem.gmapPlace.geometry === undefined;
-        validate(!invalid);
-        _inputElem.disabled = !invalid;
-        $toggle[0].checked = !invalid;
-      });
+        function setLocation(location){
+          try{
+            inputElement.$lat.val(location.lat());
+            inputElement.$lng.val(location.lng());
+          } catch(ex){
+            throw ex;
+          }
+        }
+        //endmethods
+        //events
+        google.maps.event.addListener(inputElement.gmapAutocomplete, 'place_changed', function(){
+          inputElement.gmapPlace = inputElement.gmapAutocomplete.getPlace();
+          var location = inputElement.gmapPlace.geometry === undefined ? undefined :
+                          inputElement.gmapPlace.geometry.location;
+          toggleInputs(location);
+          if(location) setLocation(location);
+        });
+        //endevents
+      //endprivates
     }
 });
