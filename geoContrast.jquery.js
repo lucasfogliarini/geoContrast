7680ece42a1bc9512bdf85cc61a95caf455b94b5
Lucas Fogliarini <lucasfogliarini@gmail.com>
2014-08-12 16:51:13 -0300

    Improvements

--- geoContrast.jquery.js
+++ geoContrast.jquery.js
@@ -4,13 +4,13 @@ $(function(){
       defaultOptions: {
         gmap_options: { componentRestrictions: { country: 'br'} },
         set:{
-          input_title: undefined,//'Wanna change? click on the pin!',
-          pin_title: 'coords',//'coords':'lat:"lat_number"; lng:"lng_number"';
+          input_title: 'Double-click to unset.',
+          pin_title: 'gmaps',//'gmaps':'Double-click to gmaps; lat:"lat_number"; lng:"lng_number"';
           pin: 'location-on-16.png'
         },
         nset:{
           input_title: undefined,
-          pin_title: 'Not set',
+          pin_title: 'Unset',
           pin: 'location-off-16.png'
         }
       }
@@ -18,19 +18,17 @@ $(function(){
 
     $.fn.geoContrast = function(options){
       var $autocompletes = this;
-
       for (var i = 0; i < $autocompletes.length; i++) {
         window.geoContrast.counter += 1;
 
         //props
         $autocompletes[i].options = $.extend(true,window.geoContrast.defaultOptions,options);
         $autocompletes[i].gmap_autocomplete = new google.maps.places.Autocomplete($autocompletes[i], $autocompletes[i].options.gmap_options);
-        $autocompletes[i].gmap_autocomplete.input = $autocompletes[i];
-        $autocompletes[i].$pin = $('<label class="pin_geoContrast"/>');
+        $autocompletes[i].gmap_autocomplete.input = $autocompletes[i];//accessibility
+        $autocompletes[i].$pin = $('<label class="pin_geo-contrast"/>');
         $autocompletes[i].$autocomplete = $($autocompletes[i]);
-        $autocompletes[i].$toggle = $('<input class="toggle_geoContrast" type="checkbox" />');
-        $autocompletes[i].$lat = $('<input class="lat_geoContrast" type="hidden" />');
-        $autocompletes[i].$lng = $('<input class="lng_geoContrast" type="hidden" />');
+        $autocompletes[i].$lat = $('<input class="lat_geo-contrast" type="hidden" />');
+        $autocompletes[i].$lng = $('<input class="lng_geo-contrast" type="hidden" />');
         //endprops
 
         //methods
@@ -43,15 +41,14 @@ $(function(){
           }
         }
 
-        $autocompletes[i].toggleInputs = function(valid){
+        $autocompletes[i].toggle = function(set){
           var input_title, input_cursor, pin, pin_title;
 
-          this.disabled = valid;
-          this.$toggle[0].checked = valid;
-          if (valid) {
+          this.disabled = set;
+          if (set) {
             pin = this.options.set.pin;
             pin_title = this.options.set.pin_title;
-            pin_title = pin_title === 'coords' ? 'lat: '+this.$lat.val()+'; lng: '+this.$lng.val() : pin_title;
+            pin_title = pin_title === 'gmaps' ? 'Double-click to gmaps; lat: '+this.$lat.val()+'; lng: '+this.$lng.val() : pin_title;
             input_title = this.options.set.input_title;
             input_cursor = 'not-allowed';
             if(input_title){
@@ -73,26 +70,17 @@ $(function(){
           this.$autocomplete.css('cursor',input_cursor);
         }
 
-        $autocompletes[i].clearLocation = function(){
-          this.toggleInputs(false);
-        }
         $autocompletes[i].isValid = function(){
           return this.$lat.val() !== '';
         }
         //endmethods
 
         //init
+        $autocompletes[i].$autocomplete.addClass('geo-contrast');
         $autocompletes[i].$autocomplete.css('padding-right','20px');
-        $autocompletes[i].$autocomplete.width($autocompletes[i].$autocomplete.width() - 20);
-        $autocompletes[i].$autocomplete.after($autocompletes[i].$pin);
         $autocompletes[i].$autocomplete.after($autocompletes[i].$lng);
         $autocompletes[i].$autocomplete.after($autocompletes[i].$lat);
-        $autocompletes[i].$autocomplete.after($autocompletes[i].$toggle);
-
-        $autocompletes[i].$toggle.attr('id','geoContrast_toggle_'+window.geoContrast.counter);
-        $autocompletes[i].$toggle.hide().change(function(){
-          $(this).prev()[0].clearLocation();
-        });
+        $autocompletes[i].$autocomplete.after($autocompletes[i].$pin);
 
         $autocompletes[i].$pin.css({
           position:'relative',
@@ -101,16 +89,29 @@ $(function(){
           padding: '0 8px',
           cursor: 'pointer'
         });
-        $autocompletes[i].$pin.attr('for','geoContrast_toggle_'+window.geoContrast.counter);
+
 
         google.maps.event.addListener($autocompletes[i].gmap_autocomplete, 'place_changed', function(){
           this.input.gmapPlace = this.getPlace();
           var location = this.input.gmapPlace.geometry === undefined ? undefined :
                           this.input.gmapPlace.geometry.location;
           if(location) this.input.setLocation(location);
-          this.input.toggleInputs(location);
+          this.input.toggle(location);
         });
-        $autocompletes[i].clearLocation();
+        $autocompletes[i].toggle(false);
+        if (window.geoContrast.counter > 0) {
+          $(document).on('dblclick','.geo-contrast',function(){
+            this.toggle(false);
+          });
+          $(document).on('dblclick','.pin_geo-contrast',function(){
+            var autocomplete = $(this).prev()[0];
+            if (autocomplete.isValid()) {
+              var lat = autocomplete.$lat.val();
+              var lng = autocomplete.$lng.val();
+              window.open('https://www.google.com/maps/@'+lat+','+lng+',12z');
+            };
+          });
+        };
         //endinit
       };
     }
